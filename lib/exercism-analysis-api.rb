ae10aae80c5648eb35ee73ab691e848e65b00c23
Jacob Richardson <jacob.ninja.dev@gmail.com>
2014-07-23 12:50:57 -0600

    API config

--- lib/exercism-analysis-api.rb
+++ lib/exercism-analysis-api.rb
@@ -2,3 +2,27 @@ require 'exercism-analysis'
 
 require File.expand_path('../exercism-analysis-api/analysis_formatter', __FILE__)
 require File.expand_path('../exercism-analysis-api/comment_table', __FILE__)
+
+require File.expand_path('../exercism-analysis-api/config', __FILE__)
+
+class Exercism
+
+  module AnalysisApi
+
+    def self.config
+      @@config ||= Config.from_yaml(config_path, comments_path)
+    end
+
+    private
+
+    def self.config_path
+      File.expand_path('../../config/exercism-analysis-api.yml', __FILE__)
+    end
+
+    def self.comments_path
+      File.expand_path('../../config/comments.yml', __FILE__)
+    end
+
+  end
+
+end
---
+++ lib/exercism-analysis-api/config.rb
@@ -0,0 +1,35 @@
+class Exercism
+  module AnalysisApi
+
+    class Config
+
+      AnalyzerConfig = Struct.new(:adapter, :analyzers)
+
+      def self.from_yaml(config_file, comments_file)
+        config = YAML.load(File.read(config_file))
+        comments = YAML.load(File.read(comments_file))
+        new(config['submission_url'], comments)
+      end
+
+      def initialize(submission_url, comments)
+        @submission_url = submission_url
+        @comments = comments
+      end
+
+      def submission
+        @submission ||= Submission.new(@submission_url)
+      end
+
+      def comments_table
+        @comments_table ||= CommentTable.new(@comments)
+      end
+
+      def analyzers
+        {'ruby' => AnalyzerConfig.new(Exercism::Adapters::Ruby, [Exercism::Analyzers::ForLoop])}
+      end
+
+    end
+
+  end
+
+end
