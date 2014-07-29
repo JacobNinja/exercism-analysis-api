861a62b180917ccdbb5c979fdba15bcc79773c74
Jacob Richardson <jacob.ninja.dev@gmail.com>
2014-07-29 13:15:32 -0600

    Post comments through Exercism services. Convert comment markdown to HTML

--- lib/exercism-analysis-api/submission.rb
+++ lib/exercism-analysis-api/submission.rb
@@ -1,39 +1,11 @@
-require 'net/http'
-require 'json'
-require 'uri'
-
 class Exercism
   module AnalysisApi
 
     class Submission
 
-      Value = Struct.new(:language, :code)
-
-      def initialize(url)
-        @url = url
-      end
-
-      def retrieve(key)
-        response = get_json("/submissions/#{key}")
-        Value.new(response['language'], response['code'])
-      end
-
-      def comment(key, text)
-        post("/submissions/#{key}/comment", text)
-      end
-
-      private
-
-      def get_json(path)
-        JSON.parse(Net::HTTP.get(make_uri(path)))
-      end
-
-      def post(path, body)
-        Net::HTTP.post_form(make_uri(path), comment: body)
-      end
-
-      def make_uri(path)
-        URI([@url, path].join)
+      def self.comment(key, user, body)
+        submission = ::Submission.find_by(key: key)
+        ::CreatesComment.create(submission.id, user, ::ConvertsMarkdownToHTML.convert(body)) if submission
       end
 
     end
--- workers/analysis_worker.rb
+++ workers/analysis_worker.rb
@@ -3,12 +3,12 @@ require 'sidekiq'
 class AnalysisWorker
   include Sidekiq::Worker
 
-  def perform(key)
-    submission = config.submission.retrieve(key)
-    with_config(submission.language) do |adapter, analyzers|
-      results = Exercism::Analysis.new(adapter.new(submission.code)).run(*analyzers)
+  def perform(key, language, code)
+    with_config(language) do |adapter, analyzers|
+      results = Exercism::Analysis.new(adapter.new(code)).run(*analyzers)
       comment = Exercism::AnalysisApi::AnalysisFormatter.new(config.comments_table).format(results.values.flatten)
-      config.submission.comment(key, comment.join("\n"))
+      user = ::User.last
+      Exercism::AnalysisApi::Submission.comment(key, user, comment.join("\n"))
     end
   end
 
